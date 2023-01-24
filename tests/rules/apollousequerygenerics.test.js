ad4a2df259961cdcaea7a12ca91cfcb62f6b220c
jfspencerAngel <james.spencer@angel.com>
2023-01-23 16:09:11 -0800

    add useQuery eslint rule

---
+++ tests/rules/apollousequerygenerics.test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const RuleTester = require('eslint').RuleTester;
+const rule = require('../../lib/rules/apollousequerygenerics');
+
+const ruleTester = new RuleTester({
+    parser: require.resolve('@typescript-eslint/parser'),
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: false,
+        },
+    },
+});
+
+const validStatements = [
+    `const a = useQuery<GraphQuery, QueryTypeVariables>()`,
+];
+
+const invalidStatements = [
+    `const a = useQuery()`,
+    `const a = useQuery<GraphQuery>()`,
+];
+
+ruleTester.run('apollousequerygenerics', rule, {
+    valid: [
+        { code: validStatements[0] },
+    ],
+    invalid: [
+        { code: invalidStatements[0], errors: [{ messageId: 'genericsrequired' }] },
+        { code: invalidStatements[1], errors: [{ messageId: 'variablestyperequired' }] },
+    ],
+});
