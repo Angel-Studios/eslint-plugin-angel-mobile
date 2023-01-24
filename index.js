5355dca3d38d8362729ea02368a4a57bc1deb5cd
jfspencerAngel <james.spencer@angel.com>
2023-01-24 14:16:43 -0800

    remove generics rule

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const rules = {
   optimizelyvardefault: require('./lib/rules/optimizelyvardefault'),
   optimizelyexpreq: require('./lib/rules/optimizelyexpreq'),
-  apollousequerygenerics: require('./lib/rules/apollousequerygenerics'),
 };
 
 module.exports = { rules };
