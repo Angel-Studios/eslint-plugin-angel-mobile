d657ad3b5c28c590fda168dd8530070c0f7856c7
Brian <9685115+applegb@users.noreply.github.com>
2023-03-15 15:54:03 -0500

    export rule

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const rules = {
   optimizelyvardefault: require('./lib/rules/optimizelyvardefault'),
   optimizelyexpreq: require('./lib/rules/optimizelyexpreq'),
+  actionsheetmanagerreqcontext: require('./lib/rules/actionsheetmanagerreqcontext'),
 };
 
 module.exports = { rules };
